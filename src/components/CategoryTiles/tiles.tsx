107f29760320dfd6a3b4d922f3ee77b19281ed15
unknown <rauboluk@gmail.com>
2023-09-12 01:03:12 +0200

    chore: tiles data

--- src/components/CategoryTiles/tiles.tsx
+++ src/components/CategoryTiles/tiles.tsx
@@ -4,6 +4,22 @@ import electricity from "../../assets/electricity.svg";
 import healthInsurance from "../../assets/health-insurance.svg";
 import homeLoans from "../../assets/home-loans.svg";
 import transactionAccounts from "../../assets/transaction-accounts.svg";
+import personalLoans from "../../assets/personal-loans.svg";
+import mobilePlans from "../../assets/mobile-plans.svg";
+import broadband from "../../assets/broadband-nbn-plans.svg";
+import superFunds from "../../assets/super-funds.svg";
+import shareTrading from "../../assets/share-trading.svg";
+import lifeInsurance from "../../assets/life-insurance.svg";
+import travelMoney from "../../assets/travel-money.svg";
+import travelDeals from "../../assets/travel-deals.svg";
+import bitcoin from "../../assets/bitcoin.svg";
+import petInsurance from "../../assets/pet-insurance.svg";
+import savingsAccounts from "../../assets/savings-accounts.svg";
+import moneyTransfers from "../../assets/intl-money-transfers.svg";
+import shoppingDeals from "../../assets/shopping-deals.svg";
+import internetTv from "../../assets/internet-tv.svg";
+import businessBanking from "../../assets/business-banking.svg";
+import shortTermLoans from "../../assets/short-term-loans.svg";
 
 export const tiles = [
   {
@@ -41,5 +57,113 @@ export const tiles = [
     src: electricity,
     alt: "Electricity & Gas",
     title: "Electricity & Gas",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: personalLoans,
+    alt: "Personal Loans",
+    title: "Personal Loans",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: mobilePlans,
+    alt: "Mobile Plans",
+    title: "Mobile Plans",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: broadband,
+    alt: "Broadband & NBN",
+    title: "Broadband & NBN",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: superFunds,
+    alt: "Super Funds",
+    title: "Super Funds",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: shareTrading,
+    alt: "Share Trading",
+    title: "Share Trading",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: lifeInsurance,
+    alt: "Life Insurance",
+    title: "Life Insurance",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: homeLoans,
+    alt: "Home Insurance",
+    title: "Home Insurance",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: travelMoney,
+    alt: "Life Insurance",
+    title: "Travel Insurance",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: travelDeals,
+    alt: "Travel Deals",
+    title: "Travel Deals",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: bitcoin,
+    alt: "Crypto",
+    title: "Crypto",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: carLoans,
+    alt: "Car Loans",
+    title: "Car Loans",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: petInsurance,
+    alt: "Pet Insurance",
+    title: "Pet Insurance",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: savingsAccounts,
+    alt: "Transaction Accounts",
+    title: "Transaction Accounts",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: moneyTransfers,
+    alt: "Money Transfers",
+    title: "Money Transfers",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: shoppingDeals,
+    alt: "Shopping Deals",
+    title: "Shopping Deals",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: internetTv,
+    alt: "Streaming",
+    title: "Streaming",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: businessBanking,
+    alt: "Business Insurance",
+    title: "Business Insurance",
+  },
+  {
+    href: "https://www.finder.com.au/",
+    src: shortTermLoans,
+    alt: "Short Term Loans",
+    title: "Short Term Loans",
   },
 ];
