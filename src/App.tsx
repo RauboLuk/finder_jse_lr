bb4a862221c5889f2c271444552fb1261a055f79
unknown <rauboluk@gmail.com>
2023-09-11 20:58:13 +0200

    feat: sectionSubheading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Section } from "./components/Section/Section";
 import { SectionContainer } from "./components/SectionContainer/SectionContainer";
 import { SectionHeader } from "./components/SectionHeader/SectionHeader";
+import { SectionSubheading } from "./components/SectionSubheading/SectionSubheading";
 
 function App() {
   return (
     <Section>
       <SectionContainer>
         <SectionHeader />
+        <SectionSubheading />
       </SectionContainer>
     </Section>
   );
---
+++ src/components/SectionSubheading/SectionSubheading.tsx
@@ -0,0 +1,12 @@
+import "./SectionSubheading.css";
+
+const SectionSubheading = () => {
+  return (
+    <div className="section__subheading">
+      Finder's team of 40+ experts will help you find the right choices in over
+      100 categories.
+    </div>
+  );
+};
+
+export { SectionSubheading };
