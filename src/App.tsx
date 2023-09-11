f88ce074e64fb9d4f91a0d1653b6f626caf0bae5
unknown <rauboluk@gmail.com>
2023-09-11 18:28:04 +0200

    feat: sectionHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
 import { Section } from "./components/Section/Section";
+import { SectionContainer } from "./components/SectionContainer/SectionContainer";
+import { SectionHeader } from "./components/SectionHeader/SectionHeader";
 
 function App() {
-  return <Section>Hello</Section>;
+  return (
+    <Section>
+      <SectionContainer>
+        <SectionHeader />
+      </SectionContainer>
+    </Section>
+  );
 }
 
 export default App;
---
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,11 @@
+import "./SectionHeader.css";
+
+const SectionHeader = () => {
+  return (
+    <div className="section__header">
+      Join 2 million+ Australians finding better
+    </div>
+  );
+};
+
+export { SectionHeader };
