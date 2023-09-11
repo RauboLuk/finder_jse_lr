5b24efcd577d8c2da4cf0ca6fe959e5ca5c0f06c
unknown <rauboluk@gmail.com>
2023-09-11 23:16:57 +0200

    feat: category tile height, text align

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { CategoryTile } from "./components/CategoryTile/CategoryTile";
+import { CategoryTiles } from "./components/CategoryTiles/CategoryTiles";
 import { Section } from "./components/Section/Section";
 import { SectionContainer } from "./components/SectionContainer/SectionContainer";
 import { SectionHeader } from "./components/SectionHeader/SectionHeader";
 import { SectionSubheading } from "./components/SectionSubheading/SectionSubheading";
-
-import creditCards from "./assets/credit-cards.svg";
 
 function App() {
   return (
@@ -13,12 +11,7 @@ function App() {
       <SectionContainer>
         <SectionHeader />
         <SectionSubheading />
-        <CategoryTile
-          href="https://www.finder.com.au/"
-          src={creditCards}
-          alt="Credit Cards"
-          title="Credit Cards"
-        />
+        <CategoryTiles />
       </SectionContainer>
     </Section>
   );
