c4b725d51eb773d32d1ddf366bc765ca5f6389aa
unknown <rauboluk@gmail.com>
2023-09-12 08:29:49 +0200

    refactor: app.css removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { CategoryTiles } from "./components/CategoryTiles/CategoryTiles";
 import { Section } from "./components/Section/Section";
 import { SectionHeader } from "./components/SectionHeader/SectionHeader";
